d0f2a0177a23ecd8ad19d9b0d981d96cdcafabb5
museum-grodno <museum.grodno@gmail.com>
2021-04-13 23:51:33 +0200

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
+import { Buffer } from "buffer";
+import { Dbf } from "dbf-reader";
+import { DataTable } from "dbf-reader/models/dbf-file";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  onFileChange(event: any) {
+    let reader = new FileReader();
+    if (event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];
+      reader.readAsArrayBuffer(file);
+      reader.onload = () => {
+        var arrayBuffer: ArrayBuffer = reader.result as ArrayBuffer;
+        if (arrayBuffer) {
+          let buffer: any = Buffer.from(arrayBuffer);
+          let datatable: DataTable = Dbf.read(buffer);
+          console.log(datatable);
+        }
+      };
+    }
+  }
 }
